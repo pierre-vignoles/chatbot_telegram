d68675eaf36c738eaa80c91150eb6377d45a60a4
pierre-vignoles <pierre.94800@gmail.com>
2021-12-15 01:34:55 +0100

    change config

--- my_config.py
+++ my_config.py
@@ -2,5 +2,5 @@ vocab_size: int = 2000
 embedding_dim: int = int(vocab_size ** 0.25)
 max_len: int = 20
 nb_epochs: int = 170
-TOKEN: str = "1907536379:AAFiBRrNjjAwTZ3HRKp360VB2BjkbFnkdgU"
+TOKEN: str = ""
 path_intents: str = "json_folder/intents_short.json"
